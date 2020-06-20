ee5ec67c69bad3ff0da5655abac3ab8b4f06b416
kuba <7o7@hush.com>
2020-06-20 03:13:11 +0200

    second

--- src/Aplikacja/util/ConnectionProvider.java
+++ src/Aplikacja/util/ConnectionProvider.java
@@ -16,29 +16,17 @@ public class ConnectionProvider {
     }
  
     public static DataSource getDataSource() {
-        try{
-            Connection myConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/serwis", "root", "metin2");
-            Statement statement = myConn.createStatement();
-            ResultSet myRs = statement.executeQuery("select * from article");
-            while(myRs.next()){
-                System.out.println( myRs.getString("name"));
+        if (dataSource == null) {
+            try {
+                Context initialContext = new InitialContext();
+                Context envContext = (Context) initialContext
+                        .lookup("java:comp/env");
+                DataSource ds = (DataSource) envContext.lookup("jdbc/webserwis");
+                dataSource = ds;
+            } catch (NamingException e) {
+                e.printStackTrace();
             }
         }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-
-//        if (dataSource == null) {
-//            try {
-//                Context initialContext = new InitialContext();
-//                Context envContext = (Context) initialContext
-//                        .lookup("java:comp/env");
-//                DataSource ds = (DataSource) envContext.lookup("jdbc/webserwis");
-//                dataSource = ds;
-//            } catch (NamingException e) {
-//                e.printStackTrace();
-//            }
-//        }
          
         return dataSource;
     }
